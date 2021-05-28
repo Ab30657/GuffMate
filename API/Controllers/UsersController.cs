9483f9e4f5d0731edb4cfd46940c1c4462f382da
Ab30657 <amar_ronaldo30657@yahoo.com>
2021-05-28 15:25:35 +0545

    dashboard UI

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,15 +19,15 @@ namespace API.Controllers
 		}
 
 		[HttpGet]
-		public ActionResult<IEnumerable<AppUser>> GetUsers()
+		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
 		{
-			return this._context.Users.ToList();
+			return await this._context.Users.ToListAsync();
 		}
 
 		[HttpGet("{id}")]
-		public ActionResult<AppUser> GetUser(int id)
+		public async Task<ActionResult<AppUser>> GetUser(int id)
 		{
-			return this._context.Users.Find(id);
+			return await this._context.Users.FindAsync(id);
 		}
 	}
 }
