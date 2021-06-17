276520a7127454ff867680b60e67fdc898af45f9
Ab30657 <amar_ronaldo30657@yahoo.com>
2021-06-17 18:03:09 +0545

    fixed routing issue, poor design choice...

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,18 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardContentComponent } from './dashboard-content/dashboard-content.component';
+import { FriendRequestsComponent } from './dashboard-content/discover-left/friend-requests/friend-requests.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { DiscoverComponent } from './dashboard-content/discover/discover.component';
+import { MessagesLeftComponent } from './dashboard-content/messages/messages-left/messages-left.component';
+import { MessagesComponent } from './dashboard-content/messages/messages.component';
+import { combineAll } from 'rxjs/operators';
 
 const routes: Routes = [
-	{ path: '', component: LoginComponent },
 	{ path: 'login', component: LoginComponent },
 	{
-		path: 'home',
+		path: 'discover',
 		component: DashboardContentComponent,
 		canActivate: [AuthGuard],
+		pathMatch: 'full',
+		children: [
+			{
+				path: '',
+				component: FriendRequestsComponent,
+				canActivate: [AuthGuard],
+				outlet: 'left',
+			},
+			{
+				path: '',
+				component: DiscoverComponent,
+				canActivate: [AuthGuard],
+				outlet: 'right',
+			},
+		],
 	},
-	{ path: '**', component: DashboardContentComponent, pathMatch: 'full' },
+	{
+		path: 'messages',
+		component: DashboardContentComponent,
+		pathMatch: 'full',
+		children: [
+			{
+				path: '',
+				component: MessagesLeftComponent,
+				canActivate: [AuthGuard],
+				outlet: 'left',
+			},
+			{
+				path: '',
+				component: MessagesComponent,
+				canActivate: [AuthGuard],
+				outlet: 'right',
+			},
+		],
+	},
 ];
 
 @NgModule({
