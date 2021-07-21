d357b489fed1a0a0545eeb1acf8faf0844e977ce
Ab30657 <amar_ronaldo30657@yahoo.com>
2021-07-21 23:59:57 +0545

    scroll bar added

--- client/src/app/user-profile/user-profile.component.ts
+++ client/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../_models/member';
 import { MembersService } from '../_services/members.service';
@@ -10,6 +10,22 @@ import { MembersService } from '../_services/members.service';
 })
 export class UserProfileComponent implements OnInit {
 	member: Member;
+	height: number = 400;
+	headerShrink: boolean = false;
+	@HostListener('scroll', ['$event'])
+	scrollHandler(event) {
+		var scrollAmount = event.target.scrollTop;
+		if (scrollAmount > 400) {
+			this.height = 100;
+		}
+		if (scrollAmount < 400) {
+			this.height = 400 - scrollAmount;
+			if (this.height < 100) {
+				this.height = 100;
+			}
+		}
+		console.log(this.height);
+	}
 	constructor(
 		private memberService: MembersService,
 		private route: ActivatedRoute
