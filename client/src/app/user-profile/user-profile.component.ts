58a28fcb3f3ff2d0deece3dcc02dcc7dcc12f56f
Ab30657 <amar_ronaldo30657@yahoo.com>
2021-12-12 23:52:28 -0600

    Delete user-profile components

--- client/src/app/user-profile/user-profile.component.ts
+++ client/src/app/user-profile/user-profile.component.ts
@@ -12,20 +12,20 @@ export class UserProfileComponent implements OnInit {
 	member: Member;
 	height: number = 400;
 	headerShrink: boolean = false;
-	@HostListener('scroll', ['$event'])
-	scrollHandler(event) {
-		var scrollAmount = event.target.scrollTop;
-		if (scrollAmount > 400) {
-			this.height = 100;
-		}
-		if (scrollAmount < 400) {
-			this.height = 400 - scrollAmount;
-			if (this.height < 100) {
-				this.height = 100;
-			}
-		}
-		// console.log(this.height);
-	}
+	// @HostListener('scroll', ['$event'])
+	// scrollHandler(event) {
+	// 	var scrollAmount = event.target.scrollTop;
+	// 	if (scrollAmount > 400) {
+	// 		this.height = 100;
+	// 	}
+	// 	if (scrollAmount < 400) {
+	// 		this.height = 400 - scrollAmount;
+	// 		if (this.height < 100) {
+	// 			this.height = 100;
+	// 		}
+	// 	}
+	// 	// console.log(this.height);
+	// }
 	constructor(
 		private memberService: MembersService,
 		private route: ActivatedRoute,
